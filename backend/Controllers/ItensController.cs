34774e0766586e7eaf895c4931ed768eb6d446bb
Vinicius S Sanchez <viniciusanchez@gmail.com>
2020-04-10 13:38:40 -0300

    Adicionado campo active
    Adicionado campo active.

--- backend/Controllers/ItensController.cs
+++ backend/Controllers/ItensController.cs
@@ -20,6 +20,27 @@ namespace APIListaCompras.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Index(int? list_id)
+        {
+            try
+            {
+                if (list_id.HasValue)
+                {
+                    var itens = await Task.FromResult(_context.Itens.Where(x => x.ListId == list_id).ToList());
+                    return Ok(itens);
+                }
+                else
+                {
+                    return BadRequest(new {error = "A Lista não foi informada"});
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {error = e.Message});
+            }
+        }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> Show(int id)
         {
@@ -44,19 +65,83 @@ namespace APIListaCompras.Controllers
         [HttpPost]
         public async Task<IActionResult> Store(Item item)
         {
-
+            try
+            {
+                if (item.IsValid)
+                {
+                    item.CreatedAt = DateTime.Now;
+                    item.UpdatedAt = DateTime.Now;
+                    await _context.Itens.AddAsync(item);
+                    await _context.SaveChangesAsync();
+                    return Ok(item);
+                }
+                else
+                {
+                    return BadRequest(new {error = item.ValidationMessagesString()});
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {error = e.Message});
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, Item item)
         {
-
+            try
+            {
+                if (item.IsValid)
+                {
+                    var itemTemp = await _context.Itens.FindAsync(id);
+                    if (itemTemp != null)
+                    {
+                        itemTemp.Description = item.Description;
+                        itemTemp.Note = item.Note;
+                        itemTemp.Quantity = item.Quantity;
+                        itemTemp.Price = item.Price;
+                        itemTemp.Active = item.Active;
+                        itemTemp.UpdatedAt = DateTime.Now;
+                        await _context.SaveChangesAsync();
+                        return Ok(itemTemp);
+                    }
+                    else
+                    {
+                        return BadRequest(new {error = "Item não encontrado"});
+                    }
+                } 
+                else
+                {
+                    return BadRequest(new {error = item.ValidationMessagesString()});
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {error = e.Message});
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-
+            try
+            {
+                var list = await _context.Lists.FindAsync(id);
+                if (list != null)
+                {
+                    _context.Remove(list);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound(new {error = "Item não encontrado"});
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {error = e.Message});
+            }
         }
         
     }
--- backend/Models/Item.cs
+++ backend/Models/Item.cs
@@ -14,19 +14,27 @@ namespace APIListaCompras.Models
 
         [Column("list_id")]
         [JsonPropertyName("list_id")]
-        public int ListId { get; set; }
+        [Required(ErrorMessage="A [Lista] deve ser informada.")]
+        public int? ListId { get; set; }
 
         [Column("description")]
+        [Required(ErrorMessage="A [Descrição] deve ser informada.")]
         public string Description { get; set; }
 
         [Column("note")]
         public string Note { get; set; }
 
         [Column("quantity")]
+        [Range(1, int.MaxValue, ErrorMessage="A [Quantidade] tem que ser maior que zero.")]
         public int Quantity { get; set; }
 
         [Column("price")]
+        [Range(double.MinValue, double.MaxValue, ErrorMessage="O [Valor] deve ser maior que zero.")]
         public Decimal Price { get; set; }
+
+        [Column("active")]
+        [Required(ErrorMessage="O campo [Ativo] deve ser informado.")]
+        public bool? Active { get; set; }
 
         [Column("created_at")]
         [JsonPropertyName("created_at")]
