0d30ee4a728e1d2141b54419ce3298dac3e7d6aa
Vinicius S Sanchez <viniciusanchez@gmail.com>
2020-04-15 14:08:16 -0300

    Corrigido import do projeto frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root'
 })
 export class AppComponent {
-  title = 'lista-compras-frontend';
+  
 }
